1598f3c9fe58dd68b92584e8c3d5eaf443d3f115
Noe Kurzawa <noekurzawa@gmail.com>
2020-02-18 18:07:34 +0100

    web is done remove padding top 80px  from about and about-hero section- change on 50px padding top in tose sections. carousels repair code for js before </body> and and change bavbar:before -66px on -60px from 992px

--- js/main.js
+++ js/main.js
@@ -12,17 +12,44 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", addShadow);
 
   // Skrypt naprawiający nawigację(zamyka się po kliknięciu w cokolwiek na mobilce)
-    $(document).click(function(event) {
-      var clickover = $(event.target);
-      var _opened = $(".navbar-collapse").hasClass("show");
-      if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-        $(".navbar-toggler").click();
+  $(document).click(function(event) {
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $(".navbar-toggler").click();
+    }
+  });
+
+  // slick.js
+  $(".team-carousel").slick({
+    // setting-name: setting-value
+    autoplay: true,
+    autoplaySpeed: 3500,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4
+        }
       }
-    });
-
-    // slick.js
-    $('.team-carousel').slick({
-      // setting-name: setting-value
-    });
-
+    ]
+  });
 });
